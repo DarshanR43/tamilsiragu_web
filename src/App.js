3bbb617bed9ac2b748aab1fdf359908b7382c4d4
Sanjay1266 <youremail@example.com>
2025-11-19 13:30:55 +0530

    The acheivements sector files have been added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import Achievements from './components/Achievements';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
-      <Footer />
+      <Achievements />
+      <Footer/>
     </div>
   );
 }
